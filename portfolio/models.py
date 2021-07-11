d2c341c01a9f1d5be32a8d6136b9825d9e25e926
mrychlicka <rychlickam@gmail.com>
2021-07-11 17:52:57 +0200

    Added __str__ to portfolio models

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,6 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)  # blank can be added everywhere
+
+    def __str__(self):
+        return self.title  # this cause that in admin/ there is no entry likr Project(1), Project(2) but real title of project
