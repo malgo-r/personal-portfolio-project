2cda7fee00282c26be3c861008506559ab13be9a
mrychlicka <rychlickam@gmail.com>
2021-07-11 13:37:16 +0200

    Registered Project model

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+admin.site.register(Project)  # hey I want to see this model inside the admin, in localhost:800/admin/ now you can see Projects table
