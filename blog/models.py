e5daaae7761ab95ce13250618d05962df2f8071d
mrychlicka <rychlickam@gmail.com>
2021-07-11 17:50:58 +0200

    Added detail view

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 
-class Blog(models.Model):
+class Blog(models.Model):  # only properties in models class need migrations. function do not
     title = models.CharField(max_length=200)
     date = models.DateField(auto_now=True)
     text = models.TextField()
     image = models.ImageField(upload_to='blog/images/', blank=True)
+
+    def __str__(self):
+        return self.title  # this cause that in admin/ there is no entry like Blog(1), Blog(2) but real title of project
