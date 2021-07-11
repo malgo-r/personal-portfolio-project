f0239b03f96683aa88b987d547c80c2479bd7dff
mrychlicka <rychlickam@gmail.com>
2021-07-11 13:31:13 +0200

    Added blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(auto_now=True)
+    text = models.TextField()
+    image = models.ImageField(upload_to='blog/images/', blank=True)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+
+def all_blogs(request):
+    entries = Blog.objects.order_by('-date')[:5]  # show only 5 latest posts
+    return render(request, 'blog/all_blogs.html', {'entries': entries})
