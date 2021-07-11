b914d78ab0bb2f697ea96c2f1d54d0323ddd059f
mrychlicka <rychlickam@gmail.com>
2021-07-11 13:36:47 +0200

    Added portfolio model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)  # https://docs.djangoproject.com/en/3.2/ref/models/fields/#field-types
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)  # blank can be added everywhere
