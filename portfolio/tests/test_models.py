6a0087ff6ca28d5cfd2b36943fb7e24b7de7402c
malgo-r <malgodev@gmail.com>
2021-09-28 22:05:37 +0200

    Added factory support to test_models in portfolio app

--- portfolio/tests/test_models.py
+++ portfolio/tests/test_models.py
@@ -1,10 +1,12 @@
 import pytest
 
-from portfolio.models import Project
+
+@pytest.mark.django_db
+def test_string_representation(project) -> None:
+    assert str(project) == "Sample project title"
 
 
 @pytest.mark.django_db
-def test_string_representation() -> None:
-    test_project = Project.objects.create(title="My test project", description="sdf")
-    assert str(test_project) == "My test project"
-    assert test_project.title == "My test project"
+def test_project_creation(project) -> None:
+    assert project.title == "Sample project title"
+    assert project.description == "Sample description"
